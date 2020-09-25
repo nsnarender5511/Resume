6eb7ee393c1f6c47ca27197dfc527a65ed710d7a
Narender <nsnarender5511@gmail.com>
2020-09-25 23:52:15 +0530

    First Commit

---
+++ build/precache-manifest.8a96414d53a2557366638ba8a83a57f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2eb54c1eb734a2ecd6b",
+    "url": "http//nsnarender5511.github.io/Portfolio/static/js/runtime~main.d2eb54c1.js"
+  },
+  {
+    "revision": "6494a672b2f66f57fc32",
+    "url": "http//nsnarender5511.github.io/Portfolio/static/js/main.6494a672.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "http//nsnarender5511.github.io/Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "6494a672b2f66f57fc32",
+    "url": "http//nsnarender5511.github.io/Portfolio/static/css/main.e13597f0.chunk.css"
+  },
+  {
+    "revision": "52e6717265456e65cf90d91718e8b9d7",
+    "url": "http//nsnarender5511.github.io/Portfolio/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,93 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Narender Singh",
+    "role": "Machine Learning Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like Playing With Data And Extracting/Predicting useful things from it and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/narender-singh-3406691a9/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/nsnarender5511",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.instagram.com/iam_narender/",
+          "className":"fa fa-instagram  "
+        }
+      ],
+    "aboutme":"I am currently a final year student at Army Institute of Technology and pursuing my B.E. in computers from here. I am a self taught Machine Learning developer, currently diving deeper into DeepLearning and Natural Language Processing. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Pune, India",
+    "website":"+91 6377005457",
+    "education":[
+      {
+        "UniversityName":"Army Institute of Technology, Pune",
+        "specialization":"Computer Science ",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":" "
+      },
+      {
+        "UniversityName":"Army Public School, Meerut Cantt.",
+        "specialization":"High School",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2016",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Machine Learning"
+      },
+      {
+        "skillname":"DeepLearning"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"https://www.skylark.com.sg/blog/wp-content/uploads/2017/03/mobile-application-development.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"https://miro.medium.com/max/640/0*8Pd-EQ87P8f0b6oQ"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"https://image.shutterstock.com/image-vector/particles-geometric-art-line-dot-260nw-1111223003.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"https://miro.medium.com/max/1200/1*sDa7Oqnh-zRXPPewKZid4g.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"I had the pleasure of working with Narender on various projects. Connecting dots of Technology and Business end to end is something which comes naturally to him.",
+        "name":"Laxman Sharma "
+      },
+      {
+        "description":"Narender is Hard working Guy. When I worked with him he had some excellent ideas about feature engineering and some good insights into validation.",
+        "name":"Nikhil Kumar Mishra"
+      }
+    ]
+  }
+  
+  export default resumeData

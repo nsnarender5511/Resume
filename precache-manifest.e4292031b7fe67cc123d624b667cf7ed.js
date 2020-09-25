3b28454bfb71e0da186f1375a424de994e1c3bc7
Narender <nsnarender5511@gmail.com>
2020-09-26 00:05:04 +0530

    Updates

---
+++ precache-manifest.e4292031b7fe67cc123d624b667cf7ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d5efb982c82b6672434",
+    "url": "/Resume/static/js/runtime~main.8d5efb98.js"
+  },
+  {
+    "revision": "d97b25d283db31510433",
+    "url": "/Resume/static/js/main.d97b25d2.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Resume/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "d97b25d283db31510433",
+    "url": "/Resume/static/css/main.e13597f0.chunk.css"
+  },
+  {
+    "revision": "1cfabc7b2071166bccc55fec620bf9d0",
+    "url": "/Resume/index.html"
+  }
+];
